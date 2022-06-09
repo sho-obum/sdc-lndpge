0f076bff87e3f23851fe1573b2459f658ae8c3e6
Shubham Kapoor <shubham28204@gmail.com>
2022-06-09 22:55:42 +0530

    Fixed Navbar toggle issue. Added Logo image and event temp images

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ function move(e){
 
 /*===== GSAP ANIMATION =====*/
 // NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+gsap.from('.nav__logo, .nav__toggle , .home__navimg', {opacity: 0, duration: 1, delay:2, y: 10})
 gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
 
 // HOME
